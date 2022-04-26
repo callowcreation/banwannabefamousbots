58c12b96d3f2f002e52e695b56226733f4e9d0e1
callowcreation <callowcreation@gmail.com>
2022-04-25 17:49:07 -0700

    added 'if you have given it mod privileges' to the famous command

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ const commands = {
 	},
 	'famous': async ({ channel, user }) => {
 		let target = channel.substr(1);
-		await client.say(target, `@${user.username} ${process.env.BOT_USERNAME} is keeping ${target} from being famous.`);
+		await client.say(target, `@${user.username} ${process.env.BOT_USERNAME} is keeping ${target} from being famous, if you have given it mod privileges.`);
 	}
 };
 
